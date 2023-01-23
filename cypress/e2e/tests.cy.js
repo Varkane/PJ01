1cac4a85462e0b4be30a7d3ade6d98cbe1a9f4a5
Ricardo <dethrattlehead@gmail.com>
2023-01-23 16:56:03 -0700

    added custom command

--- cypress/e2e/tests.cy.js
+++ cypress/e2e/tests.cy.js
@@ -2,11 +2,7 @@
 
 describe('Search elements', () => {
   it('search for elements with multiple results', () => {
-    cy.visit('/');
-    cy.fixture('index').then((index)=>{
-      cy.get(index.subscribeBox).type('Ricard@mail.com');
-      cy.get(index.subscribeButton).click();
-      cy.get(index.alert).should('contain','You have been successfully subscribed!');
+      cy.visit('/');
+      cy.search('rigar@gma.com')      
     })
   })
-})
